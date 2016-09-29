b3c7d702cb3d291260b5f00a77b4d2adb92f0d92
github username <github email address>
2016-09-29 14:34:48 -0400

    Jonathan is a god

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+require "pry"
+class School
+  attr_reader :roster
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    if @roster[grade] == nil
+      @roster[grade] = []
+    end
+      @roster[grade] << name
+  end
+
+  def grade(grade)
+    @roster[grade]
+  end
+
+  def sort
+    @roster.each_with_object(@roster.clone) do |(grade, student_array), sorted_roster|
+      sorted_roster[grade] = student_array.sort
+    end
+  end
+
+
+
+
+
+
+
+end
